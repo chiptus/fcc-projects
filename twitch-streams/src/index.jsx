584e5cd3d89e163a75d1ab8fd6e3ca7a77d21b5b
Chaim Lando <chiptus@gmail.com>
2016-11-02 15:43:24 +0530

    moved datainterface and stream to a different module

--- twitch-streams/src/index.jsx
+++ twitch-streams/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import fetchJsonp from 'fetch-jsonp';
+
+import DataInterface from './lib/data-interface';
 
 const ORG_STREAMS_NAMES = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-const API_KEY = "j2m7to7ft669avlejckfi69iroc81ax";
 const TWITCH_URL = `https://api.twitch.tv/kraken/streams`;
 const FILTER_VALUES = {
   ALL: "0",
@@ -12,84 +12,7 @@ const FILTER_VALUES = {
   OFFLINE: "2",
 };
 
-class DataInterface {
-  constructor() {
-    this.streams = [];
-  }
 
-  loadStreams(streams = []){
-    if (!streams.length){
-      return Promise.resolve([]);
-    }
-    return Promise.all(
-      streams.map(s => 
-                  fetchJsonp(`https://api.twitch.tv/kraken/channels/${s}?client_id=${API_KEY}`)
-                  .then(r => r.json())
-                  .then(ch => {
-                        if (ch.error){
-                          return new Stream(s);
-                        }
-                        return Stream.buildStream(ch);
-                      }
-                    )
-                 )
-      ).then(sts => this.streams = sts);
-    
-//     return fetchJsonp(`${TWITCH_URL}?channel=${streams.join(",")}&client_id=${API_KEY}`)
-//       .then(response => response.json())
-//       .then(results => {
-//         let online = results.streams.map(stream => Stream.buildStream(stream.channel));
-//         let offline = streams.filter(s => online.every(e => e.channelName !== s)).map(s => new Stream(s,"","",undefined,false));
-//         this.streams =     online.concat(offline);  
-//         return this.streams;
-//     });
-  }
-  
-  setStreams(streams = []) {
-    this.streams = streams;
-  }
-
-  getStreams() {
-    return this.streams;
-  }
-
-  addStream(streamName) {
-    if (!streamName){
-      return Promise.reject("missing name");
-    }
-    return fetchJsonp(`${TWITCH_URL}/${streamName}?client_id=${API_KEY}`)
-      .then(r => r.json())
-      .then(({stream:{channel}}) => {
-        let stream = new Stream(streamName, "", "", undefined, false);
-        if (channel){
-          stream = Stream.buildStream(channel);
-        }
-        this.streams.unshift(stream);
-      return stream;
-    })
-  }
-  
-  removeStream(stream){
-    this.streams = this.streams.filter(e => e.name !== stream.name);
-  }
-}
-
-class Stream {
-  constructor(name, gameName = "", url = "", logo = "http://www.free-icons-download.net/images/blocked-icon-7877.png", online = false) {
-    if (!name) {
-      throw new Error("Missing name of stream");
-    }
-    this.name = name;
-    this.gameName = gameName;
-    this.logo = logo;
-    this.url = url;
-    this.online = online;
-  }
-  
-  static buildStream(twitchStream){
-    return new Stream(twitchStream["display_name"], twitchStream.game, twitchStream.url, twitchStream.logo, true);
-  }
-}
 
 class App extends React.Component {
   constructor(props) {
