4cb2314f9f8775d24c9e36055e35bd1abbc65850
Chaim Lando <chiptus@gmail.com>
2016-11-08 15:53:57 +0530

    calculation

--- calculator/src/lib/ActionKey.js
+++ calculator/src/lib/ActionKey.js
@@ -3,7 +3,7 @@ import Key from './Key';
 export default class ActionKey extends Key {
   action(stack, prev) {
     return {
-      stack: [...stack, prev, this.text],
+      stack: [...stack, +prev, this.text],
       prev: '0',
     };
   }
--- calculator/src/lib/buttons.js
+++ calculator/src/lib/buttons.js
@@ -1,14 +1,51 @@
 import NumberKey from './NumberKey';
 import ActionKey from './ActionKey';
 
+function calculate(stack) {
+  if (stack.length === 1) {
+    return stack[0];
+  }
+  const mulIndex = stack.indexOf('X');
+  const divIndex = stack.indexOf('/');
+  if (mulIndex !== -1 && divIndex !== -1) {
+    if (mulIndex < divIndex) {
+      return calculate(stack.slice(0, mulIndex)) * calculate(stack.slice(mulIndex + 1));
+    }
+    return calculate(stack.slice(0, divIndex)) / calculate(stack.slice(divIndex + 1));
+  }
+  if (mulIndex !== -1) {
+    return calculate(stack.slice(0, mulIndex)) * calculate(stack.slice(mulIndex + 1));
+  }
+  if (divIndex !== -1) {
+    return calculate(stack.slice(0, divIndex)) / calculate(stack.slice(divIndex + 1));
+  }
+  const plusIndex = stack.indexOf('+');
+  const minusIndex = stack.indexOf('-');
+  if (plusIndex !== -1 && minusIndex !== -1) {
+    if (plusIndex < minusIndex) {
+      return calculate(stack.slice(0, plusIndex)) + calculate(stack.slice(plusIndex + 1));
+    }
+    return calculate(stack.slice(0, minusIndex)) - calculate(stack.slice(minusIndex + 1));
+  }
+  if (plusIndex !== -1) {
+    return calculate(stack.slice(0, plusIndex)) + calculate(stack.slice(plusIndex + 1));
+  }
+  if (minusIndex !== -1) {
+    return calculate(stack.slice(0, minusIndex)) - calculate(stack.slice(minusIndex + 1));
+  }
+}
+
 export default [
   {
     text: 'CE',
-    action: prev => [...prev.slice(0, prev.length - 1)],
+    action: stack => ({
+      stack,
+      prev: '0',
+    }),
   },
   {
     text: 'C',
-    action: () => [],
+    action: () => ({ stack: [], prev: '0' }),
   },
   {
     text: '<',
@@ -46,11 +83,16 @@ export default [
     action: (stack, prev) => (
       {
         stack,
-        prev: prev[prev.length - 1] !== '.' ? `${prev}.` : prev,
+        prev: prev.indexOf('.') === -1 ? `${prev}.` : prev,
       }
     ),
   },
   {
     text: '=',
+    action: (stack, prev) => ({
+      stack: [],
+      prev: calculate([...stack, +prev]),
+    })
+    ,
   },
 ];
