dd61b80369d4e4e77c5b25bb0b0c038fd2c9beef
Chaim Lando <chiptus@gmail.com>
2016-12-22 17:13:24 +0530

    ttt convert welcome to class compoentn

--- tic-tac-toe/src/comps/welcome.jsx
+++ tic-tac-toe/src/comps/welcome.jsx
@@ -1,72 +1,84 @@
 import React from 'react';
+import $ from 'jquery';
+import 'materialize-css';
 
-const Welcome = ({ router, location }) => {
-  let nameInput;
-  let symbolInput;
-  let { name = '', symbol = 'X' } = location.query;
+export default class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    const { name = '', symbol = 'X' } = this.props.location.query;
+    this.name = name;
+    this.symbol = symbol;
 
-  function submit() {
-    if (!name) {
+    this.submit = this.submit.bind(this);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeSymbol = this.onChangeSymbol.bind(this);
+  }
+
+  componentWillMount() {
+    $(document).ready(() => {
+      $('select').material_select();
+    });
+  }
+
+  onChangeName() {
+    this.name = this.nameInput.value;
+  }
+
+  onChangeSymbol() {
+    this.symbol = this.symbolInput.value;
+  }
+
+  submit() {
+    if (!this.name) {
       return;
     }
-    router.push(`/game?symbol=${symbol}&name=${name}`);  
+    this.props.router.push(`/game?symbol=${this.symbol}&name=${this.name}`);
   }
 
-  function onChangeName(d) {
-    name = nameInput.value;
-  }
-
-  function onChangeSymbol() {
-    symbol = symbolInput.value;
-  }
-
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <div>Please enter your name and choose a symbol</div>
-      <div className="row">
-        <div className="col s6 input-field">
-          <input
-            id="name"
-            ref={(c) => {
-              nameInput = c;
-              if (nameInput) {
-                nameInput.value = name;
-              }
-            }}
-            required
-            onChange={onChangeName}
-          />
-          <label htmlFor="name" className="active">Name</label>
-        </div>
-        <div className="col s6 input-field">
-          <label htmlFor="symbol" className="active">Symbol</label>
-          <select
-            ref={(c) => {
-              symbolInput = c;
-              if (symbolInput) {
-                symbolInput.value = symbol;
-              }
-            }} onChange={onChangeSymbol}
-          >
-            <option value="X">X</option>
-            <option value="O">O</option>
-          </select>
-        </div>
-        <div>
-          <button className="btn" onClick={submit}>Play</button>
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <div>Please enter your name and choose a symbol</div>
+        <div className="row">
+          <div className="col s6 input-field">
+            <input
+              id="name"
+              ref={(c) => {
+                this.nameInput = c;
+                if (this.nameInput) {
+                  this.nameInput.value = this.name;
+                }
+              }}
+              required
+              onChange={this.onChangeName}
+            />
+            <label htmlFor="name" className="active">Name</label>
+          </div>
+          <div className="col s6 input-field">
+            <label htmlFor="symbol" className="active">Symbol</label>
+            <select
+              ref={(c) => {
+                this.symbolInput = c;
+                if (this.symbolInput) {
+                  this.symbolInput.value = this.symbol;
+                }
+              }} onChange={this.onChangeSymbol}
+            >
+              <option value="X">X</option>
+              <option value="O">O</option>
+            </select>
+          </div>
+          <div className="col s12">
+            <button className="btn" onClick={this.submit}>Play</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-};
+    );
+  }
+}
 
 Welcome.propTypes = {
-
+  location: React.PropTypes.instanceOf(Object),
+  router: React.PropTypes.instanceOf(Object),
 };
-
-// $(document).ready(() => {
-    //   $('select').material_select();
-    // });
-
-export default Welcome;
