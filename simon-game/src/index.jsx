6d5cc93553b8d8b002dff41bb459fe69d0082fe1
chiptus <chiptus@gmail.com>
2016-12-28 21:16:19 +0530

    simon removes data-interface

--- simon-game/src/index.jsx
+++ simon-game/src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './comps/app';
-import DataInterface from './lib/data-interface';
 
-const dataInterface = new DataInterface();
-ReactDom.render(<App dataInterface={dataInterface} />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'));
 
