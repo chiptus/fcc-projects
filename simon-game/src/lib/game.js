1de05c099e15e592bc8adb7346aea0ab4a7e5caf
chiptus <chiptus@gmail.com>
2016-12-28 21:46:12 +0530

    adds a game class

---
+++ simon-game/src/lib/game.js
@@ -0,0 +1,30 @@
+import steps from '../steps';
+
+export default class Game {
+  constructor() {
+    this.steps = [];
+  }
+
+  generateStep() {
+    const step = randomCell(steps);
+    this.steps.push(step.id);
+    return this.steps;
+  }
+
+  checkSteps(stepsToCheck) {
+    return this.steps.length === stepsToCheck.length
+      && this.steps.every((e, i) => e === stepsToCheck[i]);
+  }
+}
+
+window.Game = Game;
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export function randomCell(arr) {
+  return arr[randomInteger(0, arr.length)];
+}
+
+// export default Game;
