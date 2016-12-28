973142ed74a7ffad5c57113d8379dc6d143e4eb9
chiptus <chiptus@gmail.com>
2016-12-28 21:45:29 +0530

    adds steps file

---
+++ simon-game/src/steps.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    color: 'yellow',
+    id: 0,
+  },
+  {
+    color: 'red',
+    id: 1,
+  },
+  {
+    color: 'blue',
+    id: 2,
+  },
+  {
+    color: 'black',
+    id: 3,
+  },
+];
