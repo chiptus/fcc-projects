a0c32061e89a13453749e0aaf12bd9613439d3e4
Chaim Lando <chiptus@gmail.com>
2016-12-28 14:14:55 +0530

    ttt adds proptypes to won

--- tic-tac-toe/src/comps/won/won.jsx
+++ tic-tac-toe/src/comps/won/won.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import WIN_TYPES from '../../constants/win-types';
 
 const Won = ({ location, params, router }) => {
-  const text = params.name === WIN_TYPES.TIE ? `${params.name} Won` : 'This is a tie';
+  const text = params.name !== WIN_TYPES.TIE ? `${params.name} Won` : 'This is a tie';
   const { name, symbol } = location.query;
 
   function restartGame() {
@@ -17,9 +17,19 @@ const Won = ({ location, params, router }) => {
   );
 };
 
-// Won.propTypes = {
-//   name: React.PropTypes.string.isRequired,
-//   restartGame: React.PropTypes.func,
-// };
+Won.propTypes = {
+  location: React.PropTypes.shape({
+    query: React.PropTypes.shape({
+      name: React.PropTypes.string,
+      symbol: React.PropTypes.string,
+    }),
+  }),
+  params: React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+  }),
+  router: React.PropTypes.shape({
+    push: React.PropTypes.func,
+  }),
+};
 
 export default Won;
