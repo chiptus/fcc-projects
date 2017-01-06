26218162ccebd35f22f0a03cbe610e02745590db
chiptus <chiptus@gmail.com>
2017-01-06 22:04:16 +0100

    simon control comp - adds more functionality

--- simon-game/src/comps/control.jsx
+++ simon-game/src/comps/control.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export default () => (
-  <div className="control">
-    <button>Check Steps</button>
-    {true ? <div>Success</div> : <div>try again</div>}
-  </div>
-);
+import StrictButton from './control/strict-button';
+
+const Control = ({ level, strict, newGame, wrongButton, toggleStrict, victory }) => {
+  if (victory) {
+    return (
+      <div className="control">
+        <div>
+          <h4 className="victory">Victory</h4>
+        </div>
+      </div>
+    );
+  }
+  if (wrongButton) {
+    return (
+      <div className="control">
+        <div>
+          <h4 className="wrong-button">Wrong Button</h4>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="control">
+      <div className="row">
+        <div className="col s4">
+          Count: {level}
+        </div>
+        <div className="col s4">
+          <button onClick={newGame}>New Game</button>
+        </div>
+        <div className="col s4">
+          <StrictButton strict={strict} toggleStrict={toggleStrict} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Control.propTypes = {
+  level: PropTypes.number.isRequired,
+  strict: PropTypes.bool.isRequired,
+  newGame: PropTypes.func.isRequired,
+  wrongButton: PropTypes.bool.isRequired,
+  toggleStrict: PropTypes.func.isRequired,
+  victory: PropTypes.bool.isRequired,
+};
+
+export default Control;
